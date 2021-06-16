ff69b97a17972a89fc1e4221f172841e5a78f86a
Rintrium <matthias@tebbe.cloud>
2021-06-16 17:31:41 +0200

    first commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"selverf adapter settings": {
+		"en": "Adapter settings for selverf",
+		"de": "Adaptereinstellungen für selverf",
+		"ru": "Настройки адаптера для selverf",
+		"pt": "Configurações do adaptador para selverf",
+		"nl": "Adapterinstellingen voor selverf",
+		"fr": "Paramètres d'adaptateur pour selverf",
+		"it": "Impostazioni dell'adattatore per selverf",
+		"es": "Ajustes del adaptador para selverf",
+		"pl": "Ustawienia adaptera dla selverf",
+		"zh-cn": "selverf的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "option1",
+		"ru": "option1",
+		"pt": "option1",
+		"nl": "option1",
+		"fr": "option1",
+		"it": "option1",
+		"es": "option1",
+		"pl": "option1",
+		"zh-cn": "option1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "option2",
+		"ru": "option2",
+		"pt": "option2",
+		"nl": "option2",
+		"fr": "option2",
+		"it": "option2",
+		"es": "option2",
+		"pl": "option2",
+		"zh-cn": "option2"
+	}
+};
