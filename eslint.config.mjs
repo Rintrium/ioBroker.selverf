c9514e4500f64b0da475b295f9d5da087fc12712
rintrium <main@rintrium.de>
2024-12-14 16:46:35 +0100

    Eslint fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,7 +17,8 @@ export default [
             'admin/build', 
             'admin/words.js',
             'admin/admin.d.ts',
-            '**/adapter-config.d.ts'     
+            '**/adapter-config.d.ts',
+            'gulpfile.js'  //remove this ignore later. Necessary for now   
         ] 
     },
 
